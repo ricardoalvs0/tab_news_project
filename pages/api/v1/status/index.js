0a89f094aa1e92167093ee30d8beeae4e810c9f6
Ricardo Alves <35077299+ricardoalvs0@users.noreply.github.com>
2024-12-17 02:31:53 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(req, res) {
-    return res.status(200).json({key: "Teste de response"})
+import database from '../../../../infra/database.js'
+
+export default async function status(req, res) {
+    const result = await database.query("SELECT 1 + 1 as sum;");
+    console.log(result.rows);
+    return res.status(200).json({key: "Teste de response"});
 }
