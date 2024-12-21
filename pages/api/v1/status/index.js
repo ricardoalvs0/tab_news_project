17a550a82ac8015b7a15bfe73db7affe18244717
Ricardo Alves <35077299+ricardoalvs0@users.noreply.github.com>
2024-12-21 13:55:56 +0000

    finish  endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,34 @@
 import database from 'infra/database.js'
 
 export default async function status(req, res) {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows);
-    return res.status(200).json({key: "Teste de response"});
+    const updatedAt = new Date().toISOString();
+
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+    const databaseMaxConnectionsResult = await database.query("SHOW max_connections;");
+    const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
+
+    const databaseName = process.env.POSTGRES_DB;
+
+    const databaseOpenedConnectionsResult = await database.query({
+        text: "SELECT count(*)::int from pg_stat_activity WHERE datname = $1;",
+        values: [databaseName]
+    })
+    const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
+
+    return res.status(200).json({
+        updated_at: updatedAt,
+        dependencies: {
+            database: {
+                postgres_version: databaseVersionValue,
+                max_connections: parseInt(databaseMaxConnectionsValue),
+                opened_connections: databaseOpenedConnectionsValue,
+            }
+        }
+    });
 }
+
+
+// Return: Postgres version, maximum number of connections, connections being used
+// const result = await database.query("SELECT 1 + 1 as sum;");
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,33 @@
+// test.only will skip all other tests on this file, and only run the current test
 test("GET to /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status")
-
     expect(response.status).toBe(200)
+
+    const responseBody = await response.json();
+
+    expect(responseBody.updated_at).toBeDefined();
+    expect(responseBody.dependencies.database.postgres_version).toBeDefined();
+    expect(responseBody.dependencies.database.opened_connections).toBeDefined();
+    expect(responseBody.dependencies.database.max_connections).toBeDefined();
+
+    expect(responseBody).toHaveProperty("dependencies");
+    expect(responseBody.dependencies).toHaveProperty("database");
+    expect(responseBody.dependencies.database).toHaveProperty("max_connections");
+    expect(responseBody.dependencies.database).toHaveProperty("opened_connections");
+    expect(responseBody.dependencies.database).toHaveProperty("postgres_version");
+
+    expect(responseBody.dependencies.database.postgres_version).toEqual("16.0");
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+
+    const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
+
+// test.only("GET to /api/v1/status should return 200", async () => {
+//     // await fetch("http://localhost:3000/api/v1/status?databaseName=local_db")
+//     // await fetch("http://localhost:3000/api/v1/status?databaseName=")
+//     // await fetch("http://localhost:3000/api/v1/status?databaseName=';")
+//     // await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4);")
+//     await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --") // -- will make everything after that be recognized as a comment (in a query)
+// });
