2a5db82f8650009e4e72dd0999eea3ad70cb5336
Ricardo Alves <35077299+ricardoalvs0@users.noreply.github.com>
2024-12-27 03:00:47 +0000

    adding files to repo to report error during migrations up

---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,30 @@
+import migrationRunner from 'node-pg-migrate';
+import { join } from 'node:path';
+
+export default async function migrations(req, res) {
+    const defaultMigrationOptions = {
+        databaseUrl: process.env.DATABASE_URL,
+        dryRun: true,
+        dir: join("infra", "migrations"),
+        direction: "up",
+        verbose: true,
+        migrationsTable: "pgmigrations"
+    }
+
+    if (req.method === 'GET') {
+        const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+        return res.status(200).json(pendingMigrations);
+    }
+
+    if (req.method === 'POST') {
+        const migratedMigrations = await migrationRunner({...defaultMigrationOptions, dryRun: false});
+
+        if (migratedMigrations.length > 0) {
+            return res.status(201).json(migratedMigrations);
+        }
+
+        return res.status(200).json(migratedMigrations);
+    }
+
+    return res.status(405).end();
+}
