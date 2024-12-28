b460524222a7f4c7422eb99d6e8979dffb17c8ee
Ricardo Alves <35077299+ricardoalvs0@users.noreply.github.com>
2024-12-28 02:38:03 +0000

    adds  endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,9 +1,12 @@
 import migrationRunner from 'node-pg-migrate';
 import { join } from 'node:path';
+import database from 'infra/database';
 
 export default async function migrations(req, res) {
+    const dbClient = await database.getNewClient()
+
     const defaultMigrationOptions = {
-        databaseUrl: process.env.DATABASE_URL,
+        dbClient: dbClient,
         dryRun: true,
         dir: join("infra", "migrations"),
         direction: "up",
@@ -13,11 +16,14 @@ export default async function migrations(req, res) {
 
     if (req.method === 'GET') {
         const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+        await dbClient.end();
         return res.status(200).json(pendingMigrations);
     }
 
     if (req.method === 'POST') {
         const migratedMigrations = await migrationRunner({...defaultMigrationOptions, dryRun: false});
+
+        await dbClient.end();
 
         if (migratedMigrations.length > 0) {
             return res.status(201).json(migratedMigrations);
