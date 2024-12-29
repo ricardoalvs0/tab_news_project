b05ef8d8589003164954ebe5fe338c1f3ecdce6a
Ricardo Alves <35077299+ricardoalvs0@users.noreply.github.com>
2024-12-29 02:38:45 +0000

    fix  endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,34 +3,46 @@ import { join } from 'node:path';
 import database from 'infra/database';
 
 export default async function migrations(req, res) {
-    const dbClient = await database.getNewClient()
-
-    const defaultMigrationOptions = {
-        dbClient: dbClient,
-        dryRun: true,
-        dir: join("infra", "migrations"),
-        direction: "up",
-        verbose: true,
-        migrationsTable: "pgmigrations"
+    const allowedMethods = ["GET", "POST"];
+    if (!allowedMethods.includes(req.method)) {
+        return res.status(405).json({
+            error: `Method ${req.method} is not allowed for this endpoint.`
+        });
     }
 
-    if (req.method === 'GET') {
-        const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-        await dbClient.end();
-        return res.status(200).json(pendingMigrations);
-    }
+    let dbClient;
 
-    if (req.method === 'POST') {
-        const migratedMigrations = await migrationRunner({...defaultMigrationOptions, dryRun: false});
+    try {
+        dbClient = await database.getNewClient()
 
-        await dbClient.end();
-
-        if (migratedMigrations.length > 0) {
-            return res.status(201).json(migratedMigrations);
+        const defaultMigrationOptions = {
+            dbClient: dbClient,
+            dryRun: true,
+            dir: join("infra", "migrations"),
+            direction: "up",
+            verbose: true,
+            migrationsTable: "pgmigrations"
         }
 
-        return res.status(200).json(migratedMigrations);
-    }
+        if (req.method === 'GET') {
+            const pendingMigrations = await migrationRunner(defaultMigrationOptions);
 
-    return res.status(405).end();
+            return res.status(200).json(pendingMigrations);
+        }
+
+        if (req.method === 'POST') {
+            const migratedMigrations = await migrationRunner({...defaultMigrationOptions, dryRun: false});
+
+            if (migratedMigrations.length > 0) {
+                return res.status(201).json(migratedMigrations);
+            }
+
+            return res.status(200).json(migratedMigrations);
+        }
+    } catch (error) {
+        console.error(error)
+        throw error;
+    } finally {
+        await dbClient.end();
+    }
 }
