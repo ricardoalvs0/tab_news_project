804ade57d51ffed1a4d176fe63a843b3724930de
Ricardo Alves <35077299+ricardoalvs0@users.noreply.github.com>
2025-01-12 15:07:00 +0000

    adds `Linting` Github Action

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -35,11 +35,3 @@ test("GET to /api/v1/status should return 200", async () => {
     const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
     expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 });
-
-// test.only("GET to /api/v1/status should return 200", async () => {
-//     // await fetch("http://localhost:3000/api/v1/status?databaseName=local_db")
-//     // await fetch("http://localhost:3000/api/v1/status?databaseName=")
-//     // await fetch("http://localhost:3000/api/v1/status?databaseName=';")
-//     // await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4);")
-//     await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --") // -- will make everything after that be recognized as a comment (in a query)
-// });
